bab1efa6ec9913b7e1905255c8c7492cfe8ca105
Sadegh_Test <sadeghdsadegh52@gmail.com>
2025-07-25 12:12:46 +0330

    favorites_page & home_page change for audio_manager

--- lib/pages/home/favorites_page.dart
+++ lib/pages/home/favorites_page.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import '../../services/favorites_service.dart';
-import '../shop/song_detail_page.dart'; // مسیر song_detail_page را تنظیم کن
+import '../../services/audio_manager.dart';
+import '../shop/song_detail_page.dart';
 
-class FavoritesPage extends StatefulWidget {
+class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
-
-  @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage> {
-  final AudioPlayer _player = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
     final favorites = FavoritesService.instance.getFavorites();
+    final audioManager = AudioManager.instance;
 
     if (favorites.isEmpty) {
       return const Center(
@@ -31,31 +25,36 @@ class _FavoritesPageState extends State<FavoritesPage> {
       itemCount: favorites.length,
       itemBuilder: (context, index) {
         final song = favorites[index];
+        final isCurrent = audioManager.player.currentIndex == index &&
+            audioManager.currentPlaylist == favorites;
+
         return ListTile(
           leading: QueryArtworkWidget(
             id: song.id,
             type: ArtworkType.AUDIO,
             nullArtworkWidget: const Icon(Icons.music_note, color: Colors.white),
           ),
-          title: Text(song.title, style: const TextStyle(color: Colors.white)),
-          subtitle: Text(song.artist ?? 'Unknown Artist', style: const TextStyle(color: Colors.grey)),
+          title: Text(
+            song.title,
+            style: TextStyle(
+              color: isCurrent ? Colors.purpleAccent : Colors.white,
+              fontWeight: isCurrent ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+          subtitle: Text(
+            song.artist ?? 'Unknown Artist',
+            style: const TextStyle(color: Colors.grey),
+          ),
           onTap: () async {
-            await _player.setAudioSource(
-              ConcatenatingAudioSource(
-                children: favorites
-                    .map((s) => AudioSource.uri(Uri.parse(s.uri!), tag: s))
-                    .toList(),
-              ),
-              initialIndex: index,
-            );
-            await _player.play();
+            audioManager.setPlaylist(favorites);
+            await audioManager.playAtIndex(index);
 
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (_) => SongDetailPage(
                   song: song,
-                  player: _player,
+                  player: audioManager.player,
                 ),
               ),
             );
@@ -63,11 +62,5 @@ class _FavoritesPageState extends State<FavoritesPage> {
         );
       },
     );
-  }
-
-  @override
-  void dispose() {
-    _player.dispose();
-    super.dispose();
   }
 }
