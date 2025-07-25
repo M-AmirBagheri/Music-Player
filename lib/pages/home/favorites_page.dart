0dd000115939c30baf7853c856fbb7c8b68e6c1b
M-AmirBagheri <mohammadamirbagheri19@gmail.com>
2025-07-25 10:57:17 +0300

    favorit page change and add audio manager page

--- lib/pages/home/favorites_page.dart
+++ lib/pages/home/favorites_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import '../../services/favorites_service.dart';
+import '../shop/song_detail_page.dart'; // مسیر song_detail_page را تنظیم کن
 
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  final AudioPlayer _player = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +39,35 @@ class FavoritesPage extends StatelessWidget {
           ),
           title: Text(song.title, style: const TextStyle(color: Colors.white)),
           subtitle: Text(song.artist ?? 'Unknown Artist', style: const TextStyle(color: Colors.grey)),
+          onTap: () async {
+            await _player.setAudioSource(
+              ConcatenatingAudioSource(
+                children: favorites
+                    .map((s) => AudioSource.uri(Uri.parse(s.uri!), tag: s))
+                    .toList(),
+              ),
+              initialIndex: index,
+            );
+            await _player.play();
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => SongDetailPage(
+                  song: song,
+                  player: _player,
+                ),
+              ),
+            );
+          },
         );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
   }
 }
