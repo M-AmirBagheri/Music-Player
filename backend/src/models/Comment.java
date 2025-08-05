90511d2fc6303c5f305857cb556bb2abbb0ebc26
Sadegh_Test <sadeghdsadegh52@gmail.com>
2025-08-05 19:40:39 +0330

    create Comment.java & Rating.java

---
+++ backend/src/models/Comment.java
@@ -0,0 +1,61 @@
+package models;
+
+public class Comment {
+    private int id;
+    private int userId;
+    private int songId;
+    private String text;
+    private int likeCount;
+    private int dislikeCount;
+    private String createdAt;
+
+    public Comment(int id, int userId, int songId, String text, int likeCount, int dislikeCount, String createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.songId = songId;
+        this.text = text;
+        this.likeCount = likeCount;
+        this.dislikeCount = dislikeCount;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public int getDislikeCount() {
+        return dislikeCount;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public void setDislikeCount(int dislikeCount) {
+        this.dislikeCount = dislikeCount;
+    }
+}
---
+++ backend/src/models/Rating.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Rating {
+    private int id;
+    private int userId;
+    private int songId;
+    private int rating;  
+
+    public Rating(int id, int userId, int songId, int rating) {
+        this.id = id;
+        this.userId = userId;
+        this.songId = songId;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
