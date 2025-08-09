c55d0595971a72e97c02dea9dc543af866e4e36d
Sadegh_Test <sadeghdsadegh52@gmail.com>
2025-08-09 21:32:13 +0330

    make MusicServer.java

---
+++ backend/src/server/MusicServer.java
@@ -0,0 +1,38 @@
+package server;
+
+import database.DatabaseManager;
+import models.Song;
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+public class MusicServer {
+    private static final int PORT = 12345;
+    private static final String SERVER_HOST = "localhost";
+    private ServerSocket serverSocket;
+    private DatabaseManager db;
+
+    public MusicServer() {
+        try {
+           
+            db = new DatabaseManager();
+            
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running on port " + PORT);
+
+            
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress());
+                new ClientHandler(clientSocket, db).start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error in MusicServer: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new MusicServer();  
+    }
+}
